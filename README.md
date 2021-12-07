f889640b10090b02a5a29486a77e40f16a9de37d
entikan <momojo@rocketship.com>
2021-12-08 00:11:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Models to use with panda3d.
+BAMs are converted with panda3d-boterham.
+License is the Panda3D License.
+
+You, make better, add models!
---
+++ setup.py
@@ -0,0 +1,26 @@
+
+"""A setuptools based setup module.
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='panda3d-assetcoop',
+    version='0.01',
+    description='Free to use panda3d models.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/thecheapestpixels/panda3d-assetcoop',
+    author='thecheapestpixels',
+    keywords='panda3d',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['panda3d'],
+)
